d3ccc30ec2ff07fd1c5e0118fa6d9835f54f2e7d
viktorderkach777 <viktorderkach777@yandex.ua>
2019-08-14 21:01:47 +0300

    captcha 2

--- SiteTest/NewFolder/src/components/captcha/reducer.js
+++ SiteTest/NewFolder/src/components/captcha/reducer.js
@@ -1,3 +1,12 @@
+import update from '../../helpers/update';
+import CaptchaService from './captchaService';
+
+
+export const KEY_POST_STARTED = "captcha/KEY_POST_STARTED";
+export const KEY_POST_SUCCESS = "captcha/KEY_POST_SUCCESS";
+export const KEY_POST_FAILED = "captcha/KEY_POST_FAILED";
+
+
 const initialState = {
     key: {
         data: null,
@@ -12,10 +21,62 @@ export const captchaReducer = (state = initialState, action) => {
 
     switch (action.type) {
 
+        case KEY_POST_STARTED: {
+            ///let {key} = state;
+            //const {data,error,loading,success} = state.key;
+            // const key = {
+            //     data: null,
+            //     success: false,
+            //     loading: true,
+            //     error: false
+            // };
+            // newState = { ...state, key };
+            //newState = { ...state, }
+            newState = update.set(state, 'key.loading', true);
+            newState = update.set(newState, 'key.success', false);
+            newState = update.set(newState, 'key.data', null);
+            break;
+        }
+
         default: {
             return newState;
         }
     }
 
-    //return newState;
+    return newState;
+}
+
+export const keyCaptchaActions = {
+    started: () => {
+        return {
+            type: KEY_POST_STARTED
+        }
+    },
+
+    success: (data) => {
+        return {
+            type: KEY_POST_SUCCESS,
+            payload: data
+        }
+    },
+
+    failed: (error) => {
+        return {
+            type: KEY_POST_FAILED
+        }
+    }
+}
+
+export const createNewKey = () => {
+    return (dispatch) => {
+        dispatch(keyCaptchaActions.started());
+
+        CaptchaService.postNewKey()
+            .then((response) => {
+                dispatch(keyCaptchaActions.success(response));
+            })
+            .catch(() => {
+                dispatch(keyCaptchaActions.failed());
+            });
+    }
 }
