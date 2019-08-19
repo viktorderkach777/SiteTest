b20a7fb363ed990e33e73f6c87a14920301bec6e
viktorderkach777 <viktorderkach777@yandex.ua>
2019-08-19 20:59:54 +0300

    captcha8/19

--- SiteTest/Controllers/CaptchaImageController.cs
+++ SiteTest/Controllers/CaptchaImageController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -28,14 +30,37 @@ namespace SiteTest.Controllers
         private const Double yFreq = 2.0 * Math.PI / ImageHight;
         private readonly IConfiguration _configuration;
         private readonly IEmailSender _emailSender;
+
         public CaptchaImageController(IConfiguration configuration,
             IEmailSender emailSender)
         {
             _configuration = configuration;
             _emailSender = emailSender;
         }
+
+
+        private GraphicsPath DeformPath(GraphicsPath path)
+        {
+            PointF[] deformed = new PointF[path.PathPoints.Length];
+            Random rng = new Random();
+            Double xSeed = rng.NextDouble() * 2 * Math.PI;
+            Double ySeed = rng.NextDouble() * 2 * Math.PI;
+
+            for (int i = 0; i < path.PathPoints.Length; i++)
+            {
+                PointF original = path.PathPoints[i];
+                Double val = xFreq * original.X + yFreq * original.Y;
+                int xOffset = (int)(xAmp * Math.Sin(val + xSeed));
+                int yOffset = (int)(yAmp * Math.Sin(val + ySeed));
+                deformed[i] = new PointF(original.X + xOffset, original.Y + yOffset);
+            }
+
+            return new GraphicsPath(deformed, path.PathTypes);
+        }
+
+
         [HttpPost("post-guid-captcha")]
-        public async Task<IActionResult> GuidCaptcha()
+        public IActionResult GuidCaptcha()
         {
             //await _emailSender.SendEmailAsync("novakvova@gmail.com", "Confirm Email",
             //   $"Please confirm your email by clicking here: " +
@@ -51,6 +76,55 @@ namespace SiteTest.Controllers
             return Ok(challengeGuid);
         }
 
+
+
+        // GET: CaptchaImage
+        [HttpGet("get-captcha/{challengeGuid}")]
+        public IActionResult Render(string challengeGuid)
+        {
+            string key = CaptchaHelper
+                .SessionKeyPrefix + challengeGuid;
+            string solution = (string)this.HttpContext.Session.GetString(key);
+            if (solution != null)
+            {
+                //Создать пустое полотно для отображения на ней CAPTCHA
+                using (Bitmap bmp = new Bitmap(ImageWidth, ImageHight))
+                using (Graphics g = Graphics.FromImage(bmp))
+                using (Font font = new Font(FontFamily, 1f))
+                {
+                    g.Clear(Background);
+                    //Выполныть пробную визуализацию для определения найлучшего размера шрифта
+                    SizeF finalSize;
+                    SizeF testSize = g.MeasureString(solution, font);
+                    float bestFontSize = Math.Min(ImageWidth / testSize.Width, ImageHight / testSize.Height) * 0.95f;
+                    using (Font finalFont = new Font(FontFamily, bestFontSize))
+                    {
+                        finalSize = g.MeasureString(solution, finalFont);
+                    }
+                    //получить путь который представляет текст, центрированый на полотне
+                    g.PageUnit = GraphicsUnit.Point;
+                    PointF textTopLeft = new PointF((ImageWidth - finalSize.Width) / 2, (ImageHight - finalSize.Height) / 2);
+                    using (GraphicsPath path = new GraphicsPath())
+                    {
+                        path.AddString(solution, new FontFamily(FontFamily), 0,
+                            bestFontSize, textTopLeft, StringFormat.GenericDefault);
+                        //
+                        //g.DrawLine()
+                        g.SmoothingMode = SmoothingMode.HighQuality;
+                        g.FillPath(Foreground, DeformPath(path));
+                        g.Flush();
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            var b = ms.ToArray();
+                            return File(b, "image/jpeg");
+                        }
+                    }
+                }
+            }
+
+            return Ok();
+        }
         //[HttpGet]
         //public IActionResult Test()
         //{  
--- SiteTest/NewFolder/src/views/Pages/Register/Register.js
+++ SiteTest/NewFolder/src/views/Pages/Register/Register.js
@@ -7,17 +7,87 @@ import get from 'lodash.get';
 import CaptchaWidget from '../../../components/captcha';
 import CaptchaService from '../../../components/captcha/captchaService';
 import * as captchaActions from '../../../components/captcha/reducer';
-
+import axios from 'axios';
 
 class Register extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+      captchaText: "",
+      done: false,
+      isLoading: false
+    }
+  }
   componentDidMount() {
     // CaptchaService.postNewKey();
     // this.props.dispatch({type: 'captcha/KEY_POST_STARTED'});
     this.props.createNewKeyCaptcha();
   }
 
+  setStateByErrors = (name, value) => {
+    if (!!this.state.errors[name]) {
+      let errors = Object.assign({}, this.state.errors);
+      delete errors[name];
+      this.setState({
+        [name]: value,
+        errors
+      });
+    } else {
+      this.setState({ [name]: value });
+    }
+  };
+
+
+  handleChange = e => {
+    this.setStateByErrors(e.target.name, e.target.value);
+  };
+
+  onSubmitForm = e => {
+    e.preventDefault();
+    const { captchaText } = this.state;
+
+    const { keyValue } = this.props.captcha;
+
+    let errors = {};
+  
+   
+    if (captchaText === "") errors.captchaText = "Поле не може бути пустим!";
+
+    const isValid = Object.keys(errors).length === 0;
+
+    if (isValid) {
+      this.setState({
+        isLoading: true
+      });
+      const model = {
+        captchaText,
+        captchaKey: keyValue
+      };
+      console.log('model send data', model);
+      var url = "https://localhost:44345/api/account/register";
+      axios.post(url, model)
+        .then(() => this.setState({
+          done: true
+        }),
+          err => {
+            //this.reloadCaptcha();
+            this.setState({
+              errors: err.response.data,
+              isLoading: false
+            })
+          }
+        );
+    } else {
+      this.setState({ errors });
+    }
+  };
+
+
   render() {
+    const { captcha } = this.props;
+
+    console.log('----props-----', this.props)
     return (
       <div className="app flex-row align-items-center">
         <Container>
@@ -25,7 +95,7 @@ class Register extends Component {
             <Col md="9" lg="7" xl="6">
               <Card className="mx-4">
                 <CardBody className="p-4">
-                  <Form>
+                  <Form onSubmit={this.onSubmitForm}>
                     <h1>Register</h1>
                     <p className="text-muted">Create your account</p>
                     <InputGroup className="mb-3">
@@ -59,8 +129,23 @@ class Register extends Component {
                       <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
                     </InputGroup>
                     <InputGroupText>
-                      <CaptchaWidget></CaptchaWidget>
+
+                      <CaptchaWidget {...captcha}></CaptchaWidget>
+
                     </InputGroupText>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="fa fa-pencil" aria-hidden="true" onClick={this.reloadCaptcha}></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text"
+                        className="form-control"
+                        id="captchaText"
+                        name="captchaText"
+                        value={this.state.captchaText}
+                        onChange={this.handleChange} />
+                    </InputGroup>
                     <Button color="success" block>Create Account</Button>
                   </Form>
                 </CardBody>
